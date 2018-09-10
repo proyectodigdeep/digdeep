362ac7e32a9f44f5b9b0a726707cad8040b86ce4
patogalindo <j.lpumas@hotmail.com>
2018-09-10 14:25:07 -0500

    prueba login

--- public/app/controllers/homeCtrl.js
+++ public/app/controllers/homeCtrl.js
@@ -12,14 +12,15 @@ angular.module('digdeepApp.homeCtrl', [])
 	function (             $scope,   $state,   userService,	  $localStorage,  $rootScope,   $interval) {
 	$scope.state = ""
 	$scope.wordSearch = ""
-	
+	console.log("******************Home******************")
 	//verificar el tipo de usuario para mostrar la barra de navegación acorde a el
-	$rootScope.$emit('checkRollUser',{done: function() {
-   	}})
-   	$scope.typeUser = $rootScope.typeUser
 	// Obtener los datos del usuario para mostrar su perfil
 	$rootScope.$emit('reloadUser',{done: function() {
 	}})
+	
+	$rootScope.$emit('checkRollUser',{done: function() {
+   	}})
+   	$scope.typeUser = $rootScope.typeUser
 	// Recargar órdenes cada 5 segundos
 	$interval(function(){
 
