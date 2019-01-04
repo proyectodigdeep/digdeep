7451021cf1d70fe05348fd17ea4f6c856679ed6e
EUGENIO <proyectodigdeep@gmail.com>
2019-01-04 01:14:39 -0600

    Update app.js

--- public/app/app.js
+++ public/app/app.js
@@ -414,13 +414,14 @@ app.run(function(lock, $rootScope, userService, $localStorage, $state) {
 
 			 			if (id_auth0.indexOf('facebook') != -1) {
 			 				console.log(profile)
-							$rootScope.$emit("openAlert", {textAlert:profile + " " + profile.name + " " + profile.email + " " + profile.picture})
+							console.log("#" + profile.name + "# #" + profile.email + "# #" + profile.picture + "#");
+							$rootScope.$emit("openAlert", {textAlert:"#" + profile.name + "# #" + profile.email + "# #" + profile.picture + "#"})
 			 				var data_user = {
 			 					name: String(profile.name),
 								email: String(profile.email),
 								roll: "user",
 								auth0Id: id_auth0,
-								urlImg: "http://res.cloudinary.com/dclbqwg59/image/upload/v1529014920/user_default.png"
+								urlImg: profile.picture
 			 				}
 			 				// Registrar el usuario que ya ha sido registrado en auth0 con una red social
 			 				userService.registerUserBySocialRed(data_user, function (usr) {
