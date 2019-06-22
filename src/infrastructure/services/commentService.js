50ef295a788e5d18d1a3ed2e5da73b2bcbb3d81b
EUGENIO <proyectodigdeep@gmail.com>
2019-06-22 01:12:36 -0500

    test

--- src/infrastructure/services/commentService.js
+++ src/infrastructure/services/commentService.js
@@ -20,6 +20,7 @@ exports.createComment= function(req,res){
 }
 
 exports.getComments = function(req, res) {
+	res.send("Comments bien");
 	commentApp.getComments(function(comments) {
 		res.status(200)
 		res.json({
